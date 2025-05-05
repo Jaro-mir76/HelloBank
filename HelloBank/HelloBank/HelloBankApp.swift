d6c917f1a497d3ce70cd7e1db992065211d41b57
Jaro(mir) J <jaromir.j@icloud.com>
2025-05-05 15:35:59 +0200

    Hello Bank

---
+++ HelloBank/HelloBank/HelloBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBankApp.swift
+//  HelloBank
+//
+//  Created by Jaromir Jagieluk on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
